2e655cb3547b4321432437a06bdbaccdad960405
Joel James <joel.james@localmed.com>
2015-04-25 17:49:25 -0500

    Run tests with coverage

--- message_parsers/utils.py
+++ message_parsers/utils.py
@@ -1,8 +1,15 @@
+import six
+
+__all__ = [
+    'force_str',
+]
+
+
 def force_str(value, encoding='utf-8'):
     """
     Forces the value to a str instance, decoding if necessary.
     """
-    if isinstance(value, bytes):
+    if six.PY3 and isinstance(value, bytes):
         return str(value, encoding)
     else:
         return value
